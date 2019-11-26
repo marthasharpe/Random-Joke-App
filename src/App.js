83c546792537c0b798374636bda0e9414396dd19
Martha Sharpe <marthasharpe2020@gmail.com>
2019-11-25 21:50:54 -0500

    merge changes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React from 'react';
 import QuoteBox from './QuoteBox';
-<<<<<<< HEAD
 import Signature from './Signature';
 
 const title = {
   color: "white",
   textAlign: "center",
   marginTop: "3rem"
-=======
-
-const container = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  background: "darkred",
-  height: 500
-}
-
-function Header() {
-  return (
-    <div class="intro">
-      <figure id="img-div">
-          <img class="image" src="stanandspidey.png" alt="Stan Lee and Spidey" title="Stan Lee gives a thumbs-up while Spidey snaps a pic in the background." style="max-width: 486px"/>
-        <figcaption id="img-caption">Stan Lee as the image of success.</figcaption>
-      </figure>
-  
-      <div class="tribute-info">  
-        <p>Stan Lee was the creator of iconic heroes and owner of a multi-billion dollar company. He married a model and they were together for nearly 70 years. He lived to be 95 and was still doing what he loved up till the end. What was his secret? Three simple things:
-        
-        1) Meet deadlines and pay the bills,
-        2) like what you do and do what you like,
-        3) and be open to getting Lucky!
-        </p>
-      </div>
-    </div>  
-  )
->>>>>>> 7fb0e13d265fca1f242856935f4c56d2137d52e0
 }
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App" style={{width: "100%", height: "100%"}}>
       <h1 style={title}>Welcome to .getJoke()</h1>
       <QuoteBox />
       <Signature />
-=======
-    <div className="app-container" style={container}>
-      <h1 class="title">Stan Lee's Secrets to Success</h1>
-      <Header />
-      <QuoteBox />
->>>>>>> 7fb0e13d265fca1f242856935f4c56d2137d52e0
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
->>>>>>> 7fb0e13d265fca1f242856935f4c56d2137d52e0
